b09d5e3cfb926ec0c1b6216ae2c3f9cc332333eb
jkulawik <darkfire.9251@gmail.com>
2020-01-18 19:03:14 +0100

    Poprawki modelu na potrzeby testowania

--- transmitData.m
+++ transmitData.m
@@ -1,7 +1,11 @@
 % DATA FOR SIMULINK trasnmitFullSim
+
 EbN0=100 %35; the bigger the better
+SNR = 12 %SNR in dB. Using instead of Eb/N0
+
 freqOffset=1000;
-Nbps=8;
+Nbps=6; %N bits per symbol
+
 symbolsMulti=5500;
 phaseNoise=-1000;%-76; the smaller the better
 % amount of errors after which transsmison stops
@@ -9,14 +13,14 @@ tgtMaxErr=1000000;
 
 %DATA FOR RLCG TRANSMISSION LINE
 % Resistance per length (ohms/m):
-Rpr=7.1429e-3;
+Rpr=45e-3;
 % Inductance per length (H/m):
 Ipr=2.5055e-7;
 % Capacitance per length (F/m):
 Cappr=5.0616e-11;
 % Conductance per length (S/m):
-Conpr=2.78e-3
+Conpr=1.2e-3
 % Frequency (Hz):
-freq=4e6;
+freq=8e6;
 % Transmission line length (m):
-length=0.1;
+length=0.115;
